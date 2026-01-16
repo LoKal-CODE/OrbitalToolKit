e06265a9638b712c0961d4aae12810cf07416e5c
LoKal-CODE <devin.grush2007@gmail.com>
2026-01-15 21:21:33 -0700

    Added src/ and startingPoint.cpp

---
+++ src/startingPoint.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    //
+    // declare some starting variables that will be used in the toolkit...
+    //
+    float spacecraft_mass; // in kgs
+    float orbit_altitude;  // in meters 
+    
+    return 0;
+
+}
