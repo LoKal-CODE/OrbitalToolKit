90fd8bbb4fdec63f87cfc1585098b3eb09f4b0c4
LoKal-CODE <devin.grush2007@gmail.com>
2026-02-10 11:53:24 -0700

    Renamed planetdata and edited reader to relfect the rename

--- src/reader.cpp
+++ src/reader.cpp
@@ -15,7 +15,7 @@ int main() {
     ifstream inFile;
 
     // Open your file
-    inFile.open("planetdata_reformatted.csv"); // change this to the name of the .csv file to be read
+    inFile.open("planetdata.csv"); // change this to the name of the .csv file to be read
 
     // Check if the file opened successfully
     // If an error is printed - make sure you file is in the same file location
