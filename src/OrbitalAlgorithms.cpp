1b78a54a710366a0a694d8ba0cf177bc5f1d1af4
LoKal-CODE <devin.grush2007@gmail.com>
2026-02-20 16:25:06 -0700

    Added OrbitalAlgortihms that will contain the necessary algorithms for the project

---
+++ src/OrbitalAlgorithms.cpp
@@ -0,0 +1,14 @@
+/*
+ *
+ * Author: Devin Grush
+ * Purpose: This script aims to provide working examples of the algorithms that will be used to calculate the necessary parameters for 2-burn Hohmann Transfers about a central body.
+ *
+ */
+
+// Pseudocode
+//
+// float calculateOrbitalVelocity( Gravitational Constant (G), Planet Mass (M), Orbital Radius/semi-major axis (r) )
+//      orbialVelocity = sqrt(G * M / r)
+//      return orbialVelocity
+//
+// 
